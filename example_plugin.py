90f1578e59a35b3d06885879cc131789324050e1
Chris Czub <chris.czub@gmail.com>
2015-07-29 00:48:51 -0400

    puglify!

--- example_plugin.py
+++ example_plugin.py
@@ -1,8 +1,12 @@
 import string
+import cStringIO
+import inspect
+import os
 
 from libmproxy.protocol.http import decoded
 
 from bs4 import BeautifulSoup
+
 
 def piglify(src):
     words = string.split(src)
@@ -19,10 +23,6 @@ def piglify(src):
         ret += ' '
     return ret.strip()
 
-def piglify_request(context, flow):
-    print "Piglifying Request"
-    with decoded(flow.request):
-        flow.request.content = str(piglify(flow.request.content).encode('utf-8'))
 
 def piglify_response(context, flow):
     with decoded(flow.response):
@@ -48,6 +48,51 @@ def piglify_response(context, flow):
 
         flow.response.content = str(soup.prettify().encode('utf-8'))
 
+
+def disable_cache(context, flow):
+    with decoded(flow.request):
+        flow.request.headers['Cache-Control'] = ['no-cache']
+        flow.request.headers['If-Modified-Since'] = []
+
+
+def puglify_image(context, flow):
+    with decoded(flow.response):
+        try:
+            print flow.response.headers.get_first('content-type', '')
+            if flow.response.headers.get_first("content-type", "").startswith("image") or flow.response.content.startswith("\xFF\xD8\xFF\xE0\x00\x10\x4A\x46\x49\x46\x00"):
+                img = cStringIO.StringIO(open(os.path.normcase(os.path.dirname(inspect.stack()[0][1]) + '/pug.jpg'), 'rb').read())
+                flow.response.content = img.getvalue()
+                flow.response.headers["content-type"] = ["image/jpeg"]
+                print "puglified"
+                print flow.response.headers
+                print len(flow.response.content)
+        except Exception, e:
+            print repr(e)
+
+
+def colorify(context, flow):
+    with decoded(flow.response):
+        try:
+            css_response = False
+            for header in flow.response.headers:
+                if header[0].lower() == 'content-type' and ('css' in header[1].lower() or 'html' in header[1].lower()):
+                    css_response = True
+            for header in flow.request.headers:
+                if header[0].lower() == 'accept' and ('css' in header[1].lower() or 'html' in header[1].lower()):
+                    css_response = True
+
+            if not css_response:
+                return
+
+            print "pinkifying css file"
+            print context.plugins
+            import re
+            pattern_hex = re.compile('#[0-9]+', re.IGNORECASE)
+            flow.response.content = pattern_hex.sub("#EA5C7A", flow.response.content)
+        except Exception, e:
+            print repr(e)
+
+
 def start(context, argv):
     def hexdump(src, length=8):
         result = []
@@ -66,13 +111,37 @@ def start(context, argv):
                                       title='Hex View Plugin',
                                       transformer=hexdump)
 
+        context.plugins.register_action('colorswitcher',
+                                        title='Color Switcher',
+                                        options=[{
+                                            'title': 'Color',
+                                            'id': 'color',
+                                            'state': {
+                                                'value': '#EA5C7A',
+                                            },
+                                            'type': 'text',
+                                        }],
+                                        actions=[{
+                                            'title': 'Colorify',
+                                            'id': 'colorify',
+                                            'state': {
+                                                'every_flow': True,
+                                            },
+                                            'possible_hooks': ['response'],
+                                        },
+                                        {
+                                            'title': 'Disable Cache',
+                                            'id': 'disable_cache',
+                                            'state': {
+                                                'every_flow': True,
+                                            },
+                                            'possible_hooks': ['request'],
+                                        }
+                                        ])
+
         context.plugins.register_action('piglify',
                                         title='Pig Latin Plugin',
-                                        actions=[{
-                                                  'title': 'Piglify Request',
-                                                  'id': 'piglify_request',
-                                                  'possible_hooks': ['request'],
-                                                 },
+                                        actions=[
                                                  {
                                                   'title': 'Piglify Response',
                                                   'id': 'piglify_response',
@@ -85,5 +154,9 @@ def start(context, argv):
                                                   'title': 'Puglify Image',
                                                   'id': 'puglify_image',
                                                   'possible_hooks': ['response'],
+                                                  'state': {
+                                                    'every_flow': True,
+                                                    },
+
                                                  },
                                                  ])
